75184686ccb1158d700cd3c3733c0d3348635fe4
stockholic <parkjjss@gmail.com>
2020-01-21 11:19:58 +0900

    Create Params.vue

---
+++ components/Params.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Param</h1>
+        <p>params : {{ $route.params }}</p>
+        <p>ex) /xxx/1234 : {{ getParam }} userId :  {{ getParam.userId }}</p>
+
+        <hr>
+
+        <h1>Query</h1>
+        <p>query : {{ $route.query }} userId :  {{ $route.query.userId }}</p>
+        <p>ex) /xxx?useId=merong</p>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        getParam(){
+            return this.$route.params
+        }
+    },
+    created() {
+        // eslint-disable-next-line no-console
+        console.log("router", this.$router)
+        // eslint-disable-next-line no-console
+        console.log("route", this.$route)
+    },
+}
+</script>
