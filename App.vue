dc0dda79854da5cab6fcf94472239871f2ad2ec2
stockholic <parkjjss@gmail.com>
2020-01-21 11:09:48 +0900

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <User></User> -->
+    <router-view ></router-view>
+  </div>
+</template> 
+
+<script>
+/* 
+import User from './components/User.vue'
+
+export default {
+  components : {
+    User
+  }
+}
+ */
+</script>
