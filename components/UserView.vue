c9bed101af12919fdc154afa1ddc306e29ffc42c
stockholic <parkjjss@gmail.com>
2020-01-21 11:21:11 +0900

    Create UserView.vue

---
+++ components/UserView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Child link  UserView</h1>
+        <p>params : {{ getParam }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        getParam(){
+            return this.$route.params
+        }
+    }
+}
+</script>
